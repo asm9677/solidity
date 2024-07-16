3879c168737c5546b0a6744856fd1df42789b55c
asm9677 <asm9677@naver.com>
2024-07-16 09:22:44 +0900

    Test5 - 0716

--- Test/Test5_0716.sol
+++ Test/Test5_0716.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 contract TEST5 {    
     /*
     숫자를 시분초로 변환하세요.
@@ -11,6 +10,25 @@ contract TEST5 {
     1000 -> 16min 40sec
     5250 -> 1hour 27min 30sec
     */
+
+    function toBytes(uint _n) public pure returns(bytes memory res) {        
+        uint length;
+        uint tmp = _n;
+
+        while (tmp != 0) {
+            tmp /= 10;
+            length += 1;
+        }       
+
+        if(length == 0) 
+            length = 1;
+            
+        res = new bytes(length);
+        for(uint i = length; i > 0; i--) {
+            res[i-1] = bytes1(uint8(_n % 10) + 0x30);
+            _n /= 10;
+        }
+    }
 
     function getTime(uint _s) public pure returns(string memory hms) {
         uint _hours;
@@ -22,13 +40,13 @@ contract TEST5 {
         _seconds = _s % 1 minutes;
 
         if(_hours > 0) {
-            hms = string(abi.encodePacked(Strings.toString(_hours), "hour "));
+            hms = string(abi.encodePacked(toBytes(_hours), "hour "));
         }
         if(_minutes > 0) {
-            hms = string(abi.encodePacked(hms, Strings.toString(_minutes), "min "));
+            hms = string(abi.encodePacked(hms, toBytes(_minutes), "min "));
         }        
         if(_seconds > 0) {
-            hms = string(abi.encodePacked(hms, Strings.toString(_seconds), "sec"));        
+            hms = string(abi.encodePacked(hms, toBytes(_seconds), "sec"));        
         }
     }    
 }
